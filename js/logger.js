4d64340780dd52fb0e94f262a7d051918bec04a9
Dom <dominic.faryna@gmail.com>
2021-03-10 13:00:23 -0500

    Removed dumb loggers that broke dumb raspberry pi

--- js/logger.js
+++ js/logger.js
@@ -23,13 +23,13 @@
 		info: Function.prototype.bind.call(console.info, console),
 		log: Function.prototype.bind.call(console.log, console),
 		error: Function.prototype.bind.call(console.error, console),
-		warn: Function.prototype.bind.call(console.warn, console),
-		group: Function.prototype.bind.call(console.group, console),
-		groupCollapsed: Function.prototype.bind.call(console.groupCollapsed, console),
-		groupEnd: Function.prototype.bind.call(console.groupEnd, console),
-		time: Function.prototype.bind.call(console.time, console),
-		timeEnd: Function.prototype.bind.call(console.timeEnd, console),
-		timeStamp: Function.prototype.bind.call(console.timeStamp, console)
+		warn: Function.prototype.bind.call(console.warn, console)
+		//group: Function.prototype.bind.call(console.group, console),
+		//groupCollapsed: Function.prototype.bind.call(console.groupCollapsed, console),
+		//groupEnd: Function.prototype.bind.call(console.groupEnd, console),
+		//time: Function.prototype.bind.call(console.time, console),
+		//timeEnd: Function.prototype.bind.call(console.timeEnd, console),
+		//timeStamp: Function.prototype.bind.call(console.timeStamp, console)
 	};
 
 	logLevel.setLogLevel = function (newLevel) {
